b424d357d2c1fea8709dbdd242a413c171c6c3b1
Hema-0808 <129247818+Hema-0808@users.noreply.github.com>
2023-03-29 11:53:32 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Sample
+{
+public static void main (String args[])
+{
+int n,i,j;
+n=7;
+for(i=1;i<=n;i++)
+{
+System.out.print("*");
+for(j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+}
+}
+}
