b3fc5b142910889b90318d10321e396be2c4cdcf
Hema-0808 <129247818+Hema-0808@users.noreply.github.com>
2023-03-29 11:44:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main{
+  public static void main (String[] args)
+  {
+    int n,i,j;
+    n=6;
+    for(i=1;i<=n;i++)
+    {
+    for(j=1;j<=i;j++)
+    {
+    System.out.print("*");
+    }
+    System.out.println();
+    }
+    }
+  }
